caf20f777f8c371a8b80563730405e47a7fdbf86
Влад Салтанов <vladsaltanov166@gmail.com>
2024-10-12 20:18:14 +0300

    Added hello.pas

---
+++ hello.pas
@@ -0,0 +1,9 @@
+﻿##
+var user_choice := ReadInteger('Введите ваш язык (1 - русский, 2 - английский, 3 - испанский):');
+Assert(user_choice in 1..3);
+
+case user_choice of
+  1: print ('Привет!');
+  2: print ('Hello!');
+  3: print ('¡Hola!');
+end;
